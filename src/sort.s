3686e2b4bec94148a81bb6f4e0914dab14ab2b8e
artmotika <artmotika@gmail.com>
2020-10-16 16:58:46 +0300

    With help from I.Kirilenko

--- src/sort.s
+++ src/sort.s
@@ -33,11 +33,6 @@ L4:
 	jne	L3
 L1:
 	ret
-LFE0:
-	.section __TEXT,__eh_frame,coalesced,no_toc+strip_static_syms+live_support
-EH_frame1:
-	.set L$set$0,LECIE1-LSCIE1
-	.long L$set$0
 LSCIE1:
 	.long	0
 	.byte	0x1
@@ -52,18 +47,3 @@ LSCIE1:
 	.byte	0x8
 	.byte	0x90
 	.byte	0x1
-	.align 3
-LECIE1:
-LSFDE1:
-	.set L$set$1,LEFDE1-LASFDE1
-	.long L$set$1
-LASFDE1:
-	.long	LASFDE1-EH_frame1
-	.quad	LFB0-.
-	.set L$set$2,LFE0-LFB0
-	.quad L$set$2
-	.byte	0
-	.align 3
-LEFDE1:
-	.ident	"GCC: (Homebrew GCC 10.2.0) 10.2.0"
-	.subsections_via_symbols
