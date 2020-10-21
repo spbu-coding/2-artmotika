cf140857061966e746b3242601da2d08c5f1aa7c
artmotika <artmotika@gmail.com>
2020-10-21 13:03:03 +0300

    change

--- src/main.c
+++ src/main.c
@@ -1,122 +1,73 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_COUNT_OF_NUMBERS 100
 
 #ifndef __APPLE__
 # define sort_array _sort_array
 #endif
 
-#define MAX_COUNT_OF_NUMBERS 100
+extern void sort_array(int reduced_numbers_sorted[], int count_of_reduced_elements);
 
-extern void sort_array(long long reduced_numbers_sorted[], int count_of_reduced_elements);
-
-struct interval_t {
-    long long from;
-    long long to;
+struct interval_t{
+    int from_interaval;
+    int to_interval;
 };
 
-int check_arguments(int argc, char* argv[]) {
+int check_arguments(int argc, char* argv[], struct interval_t interval, int* count_from, int* count_to){
     if (argc < 2) {
         return -1;
-    }
-    if (argc > 3) {
+    } else if (argc > 3) {
         return -2;
     }
-    if (argc  == 3) {
-        if (!(strncmp(argv[1], "--to=", 5) == 0 || strncmp(argv[1], "--from=", 7) == 0) &&
-        !(strncmp(argv[2], "--to=", 5) == 0 || strncmp(argv[2], "--from=", 7) == 0)){
-            return -4;
-        }
-        if ((strncmp(argv[1], argv[2], 5) == 0) || (strncmp(argv[1], argv[2], 7) == 0)){
-            return -3;
-        }
-    }
-    if (argc == 2) {
-        if (!(strncmp(argv[1], "--to=", 5) == 0 || strncmp(argv[1], "--from=", 7) == 0)) {
-            return -4;
-        }
-    }
-    return 0;
-}
-
-void scan_arguments(int argc, char* argv[], struct interval_t* interval, int* change_return_from, int* change_return_to) {
-    long long from = 0, to = 0;
     for (int i = 0; i < argc; i++) {
-        if (strncmp(argv[i], "--to=", 5) == 0 || strncmp(argv[i], "--from=", 7) == 0) {
-            if (strncmp(argv[i], "--from=", 5) == 0) {
-                if (strtoll(argv[i] + 7, NULL, 10)) {
-                    from = strtoll(argv[i] + 7, NULL, 10);
-                    *change_return_from = 1;
-                }
-            }
-            if (strncmp(argv[i], "--to=", 5) == 0) {
-                if (strtoll(argv[i] + 5, NULL, 10)) {
-                    to = strtoll(argv[i] + 5, NULL, 10);
-                    *change_return_to = 1;
-                }
-            }
-
+        if (strncmp(argv[i], "--from=", strlen("--from=")) == 0) {
+            interval.from_interaval = strtoll(argv[i] + strlen("--from="), NULL, 10);
+            *count_from ++;
+        } else if (strncmp(argv[i], "--to=", strlen("--to=")) == 0) {
+            interval.to_interval = strtoll(argv[i] + strlen("--to="), NULL, 10);
+            *count_to ++;
         }
     }
-    interval->from = from;
-    interval->to = to;
-}
-
-void read_array(long long array[], int* count_of_elements) {
-    char c;
-    int i = 0;
-    scanf("%lli%c", &array[i], &c);
-    while (c != '\n') {
-        i++;
-        scanf("%lli%c", &array[i], &c);
+    if (*count_from > 1 || *count_to > 1) {
+        return -3;
+    } else if (*count_from == 0 && *count_to == 0) {
+        return -4;
     }
-    *count_of_elements = i + 1;
 }
 
-void reduce_numbers(long long array_of_numbers[], long long Stdout[], long long reduced_numbers[],
-                        int count_of_elements, long long Stderr[], struct interval_t interval, int* count_of_Stderr,
-                        int* count_of_Stdout, int* reduced_count, int change_return_from, int change_return_to) {
-    int count_Stdout = 0;
-    int count_Stderr = 0;
-    int count_reduced = 0;
-    for (int i = 0; i < count_of_elements; i++) {
-        if (array_of_numbers[i] <= interval.from && change_return_from) {
-            Stdout[count_Stdout] = array_of_numbers[i];
-            count_Stdout++;
+void reduce_array(int array_numbers[], int* length_array, int array_numbers_copy[], struct interval_t interval,
+        int count_from, int count_to){
+    for (int k = 0; k < MAX_COUNT_OF_NUMBERS; k++) {
+        int number;
+        char white_space;
+        if (scanf("%d%c", &number, &white_space) != 2) {
+            break;
         }
-        if (array_of_numbers[i] >= interval.to && change_return_to) {
-            Stderr[count_Stderr] = array_of_numbers[i];
-            count_Stderr++;
+        int is_number_in_interval = 0;
+        if (count_from == 1 && number <= interval.from_interaval) {
+            fprintf(stdout, "%d ", number);
+            is_number_in_interval ++;
         }
-        if ((array_of_numbers[i] > interval.from && change_return_from && (array_of_numbers[i] < interval.to || !change_return_to)) ||
-        (array_of_numbers[i] < interval.to && change_return_to && !change_return_from)){
-            reduced_numbers[count_reduced] = array_of_numbers[i];
-            count_reduced++;
+        if (count_to == 1 && number >= interval.to_interval) {
+            fprintf(stderr, "%d ", number);
+            is_number_in_interval ++;
+        }
+        if (!is_number_in_interval) {
+            array_numbers[*length_array] = number;
+            array_numbers_copy[*length_array] = number;
+            *length_array++;
+        }
+        if (white_space == '/n') {
+            break;
         }
     }
-    *count_of_Stdout = count_Stdout;
-    *count_of_Stderr = count_Stderr;
-    *reduced_count = count_reduced;
 }
 
-void copy_array(long long copy_from[], long long copy_to[], int count_of_elements) {
-    for (int i = 0; i < count_of_elements; i++) {
-        copy_to[i] = copy_from[i];
-    }
-}
-
-void display_std(long long Stdout[], int count_of_Stdout, long long Stderr[], int count_of_Stderr) {
-    for (int i = 0; i < count_of_Stdout; i++) {
-        fprintf(stdout, "%lli ", Stdout[i]);
-    }
-    for (int i = 0; i < count_of_Stderr; i++) {
-        fprintf(stderr, "%lli ", Stderr[i]);
-    }
-}
-
-int compare_arrays(long long array1[], long long array2[], int count_of_reduced_elements) {
+int compare_arrays(int array1[], int array2[], int length_array) {
     int difference_count = 0;
-    for(int i = 0; i < count_of_reduced_elements; i++) {
+    for(int i = 0; i < length_array; i++) {
         if(array1[i] != array2[i])
             difference_count++;
     }
@@ -124,27 +75,12 @@ int compare_arrays(long long array1[], long long array2[], int count_of_reduced_
 }
 
 int main(int argc, char* argv[]) {
-    struct interval_t interval = {0, 0};
-
-    long long array_of_numbers[MAX_COUNT_OF_NUMBERS], Stdout[MAX_COUNT_OF_NUMBERS], Stderr[MAX_COUNT_OF_NUMBERS],
-    reduced_numbers[MAX_COUNT_OF_NUMBERS], reduced_numbers_sorted[MAX_COUNT_OF_NUMBERS];
-
-    int count_of_elements = 0, count_of_Stdout = 0, count_of_Stderr = 0, count_of_reduced_elements = 0;
-
-    int check_arguments_return_code = check_arguments(argc, argv);
-    if (check_arguments_return_code) {
-        return check_arguments_return_code;
-    }
-    int change_return_from = 0;
-    int change_return_to = 0;
-    scan_arguments(argc, argv, &interval, &change_return_from, &change_return_to);
-    read_array(array_of_numbers, &count_of_elements);
-    reduce_numbers(array_of_numbers, Stdout, reduced_numbers, count_of_elements, Stderr, interval, &count_of_Stderr,
-                   &count_of_Stdout, &count_of_reduced_elements, change_return_from, change_return_to);
-    copy_array(reduced_numbers, reduced_numbers_sorted, count_of_reduced_elements);
-    sort_array(reduced_numbers_sorted, count_of_reduced_elements);
-
-    int count_of_changed_elements = compare_arrays(reduced_numbers, reduced_numbers_sorted, count_of_reduced_elements);
-    display_std(Stdout, count_of_Stdout, Stderr, count_of_Stderr);
-    return count_of_changed_elements;
+    struct interval_t interval;
+    int array_numbers[MAX_COUNT_OF_NUMBERS], array_numbers_copy[MAX_COUNT_OF_NUMBERS], length_array = 0;
+    int count_from = 0, count_to = 0;
+    check_arguments(argc, argv, interval, &count_from, &count_to);
+    reduce_array(array_numbers, &length_array, array_numbers_copy, interval, count_from, count_to);
+    sort_array(array_numbers, length_array);
+    int changed_elements = compare_arrays(array_numbers, array_numbers_copy, length_array);
+    return changed_elements;
 }
--- src/sort.s
+++ src/sort.s
@@ -7,25 +7,25 @@ LFB0:
 	movq	%rdi, %r8
 	testl	%edx, %edx
 	jle	L1
-	leaq	8(%rdi), %r9
+	leaq	4(%rdi), %r9
 	.p2align 4,,10
 	.p2align 3
 L3:
 	subl	$1, %edx
 	movq	%r8, %rax
-	leaq	(%r9,%rdx,8), %rsi
+	leaq	(%r9,%rdx,4), %rsi
 	movq	%rdx, %rdi
 	.p2align 4,,10
 	.p2align 3
 L5:
-	movq	(%rax), %rdx
-	movq	8(%rax), %rcx
-	cmpq	%rcx, %rdx
+	movl	(%rax), %edx
+	movl	4(%rax), %ecx
+	cmpl	%ecx, %edx
 	jle	L4
-	movq	%rcx, (%rax)
-	movq	%rdx, 8(%rax)
+	movl	%ecx, (%rax)
+	movl	%edx, 4(%rax)
 L4:
-	addq	$8, %rax
+	addq	$4, %rax
 	cmpq	%rax, %rsi
 	jne	L5
 	testl	%edi, %edi
@@ -33,6 +33,11 @@ L4:
 	jne	L3
 L1:
 	ret
+LFE0:
+	.section __TEXT,__eh_frame,coalesced,no_toc+strip_static_syms+live_support
+EH_frame1:
+	.set L$set$0,LECIE1-LSCIE1
+	.long L$set$0
 LSCIE1:
 	.long	0
 	.byte	0x1
@@ -47,4 +52,18 @@ LSCIE1:
 	.byte	0x8
 	.byte	0x90
 	.byte	0x1
-
+	.align 3
+LECIE1:
+LSFDE1:
+	.set L$set$1,LEFDE1-LASFDE1
+	.long L$set$1
+LASFDE1:
+	.long	LASFDE1-EH_frame1
+	.quad	LFB0-.
+	.set L$set$2,LFE0-LFB0
+	.quad L$set$2
+	.byte	0
+	.align 3
+LEFDE1:
+	.ident	"GCC: (Homebrew GCC 10.2.0) 10.2.0"
+	.subsections_via_symbols
